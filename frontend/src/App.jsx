ec14b0a9e1c25f8117f53c739f37ee7c46f342fc
StackBuilder-Pro <codestacker.dev.com>
2026-01-29 00:31:47 +0100

    complete frontend implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Dashboard from './pages/owner/Dashboard'
 import AddCar from './pages/owner/AddCar'
 import ManageCars from './pages/owner/ManageCars'
 import ManageBooking from './pages/owner/ManageBooking'
+import Login from './components/Login'
 
 const App = () => {
 
@@ -20,6 +21,8 @@ const App = () => {
 
   return (
     <>
+    {showLogin && <Login setShowLogin={setShowLogin}/>}
+      
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
       
       <Routes>
