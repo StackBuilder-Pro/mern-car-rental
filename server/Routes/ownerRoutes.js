4bb644a50a7e83d8f9fdbadc8afdff5fc57f7dad
StackBuilder-Pro <codestacker.dev.com>
2026-01-31 16:55:29 +0100

    Finalize ownerRoutes

--- server/Routes/ownerRoutes.js
+++ server/Routes/ownerRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import protect from '../middleware/auth.js';
-import { changRoleToOwner } from '../controllers/ownerController.js';
+import { addCar, changRoleToOwner, deleteCar, getDashboardData, getOwnerCars, toggleCarAvailability } from '../controllers/ownerController.js';
+import upload from '../middleware/multer.js';
 
 const ownerRouter = express.Router()
 
 ownerRouter.post('/change-role', protect, changRoleToOwner);
+ownerRouter.post('/add-car', upload.single('image') ,protect, addCar);
+ownerRouter.get('/cars' ,protect, getOwnerCars);
+ownerRouter.post('/delete-car',protect, deleteCar);
+ownerRouter.post('/toggle-car',protect, toggleCarAvailability);
+ownerRouter.get('/dashboard',protect, getDashboardData);
 
 
 export default ownerRouter;
